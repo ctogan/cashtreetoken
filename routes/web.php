39976a654589fca1c5c57f8a8637c6c32d516e22
morgan <morgan>
2022-12-07 15:53:01 +0700

    style run

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,27 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/faq', function () {
+    return view('faq');
+});
+
+Route::get('/download/whitepaper', function () {
+    $file= public_path(). "/whitepaper/Cashtree_Whitepaper_Public_Dec2022.pdf";
+    $headers = array(
+              'Content-Type: application/pdf',
+            );
+
+    return Response::download($file, 'Cashtree_Whitepaper.pdf', $headers);
+});
+
+Route::get('/policy', function () {
+    return view('policy');
+});
+
+Route::get('setlang/{locale}',function($lang){
+    \Session::put('locale',$lang);
+    return redirect()->back();
 });
